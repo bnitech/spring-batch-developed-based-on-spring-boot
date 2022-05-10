e11b9aefa547a14e7c2994a03d68f3514d978e34
Dongbin <bnitech88@gmail.com>
2022-05-10 19:56:49 +0900

    [dongbin] init project

--- src/main/java/com/bnitech/springbatchdevelopedbasedonspringboot/SpringBatchDevelopedBasedOnSpringBootApplication.java
+++ src/main/java/com/bnitech/springbatchdevelopedbasedonspringboot/SpringBatchDevelopedBasedOnSpringBootApplication.java
@@ -1,13 +1,15 @@
 package com.bnitech.springbatchdevelopedbasedonspringboot;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringBatchDevelopedBasedOnSpringBootApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBatchDevelopedBasedOnSpringBootApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBatchDevelopedBasedOnSpringBootApplication.class, args);
+  }
 
 }
