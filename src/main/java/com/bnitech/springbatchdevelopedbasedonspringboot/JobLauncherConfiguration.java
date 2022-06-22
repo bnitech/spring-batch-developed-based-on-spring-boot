4c011a0c945a23ceffad0cecafee0af2f9eece6a
Dongbin <dongbin.im@nhnfashiongo.com>
2022-06-23 06:46:27 +0900

    [dongbin] #3.11.1

--- src/main/java/com/bnitech/springbatchdevelopedbasedonspringboot/JobLauncherConfiguration.java
+++ src/main/java/com/bnitech/springbatchdevelopedbasedonspringboot/JobLauncherConfiguration.java
@@ -2,7 +2,6 @@ package com.bnitech.springbatchdevelopedbasedonspringboot;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -13,47 +12,42 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 @RequiredArgsConstructor
-public class ExecutionContextConfiguration {
+public class JobLauncherConfiguration {
 
   private final JobBuilderFactory jobBuilderFactory;
   private final StepBuilderFactory stepBuilderFactory;
-  private final JobExecutionListener jobExecutionListener;
 
   @Bean
   public Job BatchJob() {
-    return jobBuilderFactory.get("batchJob")
+    return this.jobBuilderFactory.get("job")
         .start(step1())
         .next(step2())
-        .listener(jobExecutionListener)
         .build();
   }
 
   @Bean
   public Step step1() {
-    return stepBuilderFactory.get("step1")
+    return this.stepBuilderFactory.get("step1")
         .tasklet(new Tasklet() {
           @Override
           public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext)
               throws Exception {
             return RepeatStatus.FINISHED;
           }
-        })
-        .build();
+        }).build();
   }
 
   @Bean
   public Step step2() {
-    return stepBuilderFactory.get("step2")
+    return this.stepBuilderFactory.get("step2")
         .tasklet(new Tasklet() {
           @Override
           public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext)
               throws Exception {
             return RepeatStatus.FINISHED;
           }
-        })
-        .build();
+        }).build();
   }
 }
